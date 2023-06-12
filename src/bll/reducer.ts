833f8359d5438f3ec94f7dcd9c5164fec4a43cc9
andbudha <andreibartov@gmail.com>
2023-06-12 22:24:23 +0200

    ResetCounterAC added

--- src/bll/reducer.ts
+++ src/bll/reducer.ts
@@ -4,11 +4,15 @@ const initialState = {
 }
 
 type InitialStateType = typeof initialState;
-type ActionType = IncreaseCounterACType;
+type ActionType = IncreaseCounterACType | ResetCounterACType;
 export const CounterReducer = (state: InitialStateType = initialState, action: ActionType): InitialStateType => {
     switch (action.type) {
         case "INCREASE-COUNTER": {
             return { ...state, value: state.value + 1 };
+        }
+        case "RESET-COUNTER": {
+            const currentValue = state.value;
+            return { ...state, value: state.value - currentValue };
         }
         default: {
             return state;
@@ -20,5 +24,13 @@ type IncreaseCounterACType = ReturnType<typeof IncreaseCounterAC>
 export const IncreaseCounterAC = () => {
     return {
         type: 'INCREASE-COUNTER'
+    } as const
+}
+
+
+type ResetCounterACType = ReturnType<typeof ResetCounterAC>
+export const ResetCounterAC = () => {
+    return {
+        type: 'RESET-COUNTER'
     } as const
 }

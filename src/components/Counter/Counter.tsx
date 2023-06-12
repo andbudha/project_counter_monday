81df301c8356506dbac8a0467ab431f64dc85e6e
andbudha <andreibartov@gmail.com>
2023-06-12 22:33:21 +0200

    useSelector&useDispatch hooks applied

--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -1,27 +1,29 @@
-import React, {useState} from 'react';
-import {Button} from "../Button/Button";
+import React, { useState } from 'react';
+import { Button } from "../Button/Button";
 import style from './Counter.module.css'
+import { useDispatch, useSelector } from 'react-redux';
+import { AppRootStateType } from '../../bll/store';
+import { IncreaseCounterAC, ResetCounterAC } from '../../bll/reducer';
 
 export const Counter = () => {
 
-    //counter state
-    const[count, setCount]=useState(0);
+    const count = useSelector<AppRootStateType, number>(state => state.counter.value);
 
+    const dispatch = useDispatch();
 
     const increaseCounterHandler = () => {
-        setCount(count+1);
-
+        dispatch(IncreaseCounterAC());
     }
 
     const resetCounterHandler = () => {
-        setCount(0);
+        dispatch(ResetCounterAC());
     }
     return (
         <div className={style.counter_container}>
             <div className={`${style.counter_display} ${count === 5 ? style.result_color : ''}`}>{count}</div>
             <div className={style.button_container}>
-                <Button title={'Increase'} callBack={increaseCounterHandler} id={1} count={count}/>
-                <Button title={'Reset'} callBack={resetCounterHandler} id={2} count={count}/>
+                <Button title={'Increase'} callBack={increaseCounterHandler} id={1} count={count} />
+                <Button title={'Reset'} callBack={resetCounterHandler} id={2} count={count} />
             </div>
         </div>
     );
